98cca7c372fbb20107e44820ec4e1fe051bbf5e1
Erica Wright <ericaw21@gmail.com>
2017-03-12 09:47:28 -0700

    create constructor, save, get and deleteAll for Box class

--- src/Box.php
+++ src/Box.php
@@ -5,8 +5,44 @@
         public $id;
         public $color;
         public $shape;
+        public $time;
+        public $points;
 
+        function __construct($color, $shape, $time, $points, $id = null)
+        {
+            $this->color = $color;
+            $this->shape = $shape;
+            $this->time = $time;
+            $this->points = $points;
+            $this->id = $id;
+        }
 
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO boxes (color, shape, view_time, points) VALUES ('{$this->color}', '{$this->shape}', {$this->time}, {$this->points});");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_boxes = $GLOBALS['DB']->query("SELECT * FROM boxes ORDER BY color;");
+            $all_boxes = array();
+            foreach($returned_boxes as $box) {
+                $color = $box['color'];
+                $shape = $box['shape'];
+                $time = $box['view_time'];
+                $points = $box['points'];
+                $id = $box['id'];
+                $new_box = new Box($color, $shape, $time, $points, $id);
+                array_push($all_boxes, $new_box);
+            }
+            return $all_boxes;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM boxes;");
+        }
 
 
     }
--- tests/BoxTest.php
+++ tests/BoxTest.php
@@ -5,7 +5,37 @@
     * @backupStaticAttributes disabled
     */
 
+    require_once 'src/Box.php';
+    require_once 'src/Score.php';
 
+    $server = 'mysql:host=localhost:8889;dbname=color_clicker_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
 
+    class BoxTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Box::deleteAll();
+            Score::deleteAll();
+        }
+
+        function test_saveAndGetAll()
+        {
+            $color = 'red';
+            $shape = 'square';
+            $time = 1500;
+            $points = 2;
+            $id = null;
+            $new_box = new Box($color, $shape, $time, $points);
+            $new_box->save();
+
+            $result = Box::getAll();
+
+            $this->assertEquals([$new_box], $result);
+        }
+
+    }
 
 ?>
